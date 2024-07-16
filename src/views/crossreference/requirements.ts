d4f88ffdce8904e58b2d2466cdef4d76cc94f69c
Jared Pon <jpon@Jareds-MacBook-Pro.local>
2024-07-16 14:57:32 -0600

    Update PDFs

--- src/views/crossreference/requirements.ts
+++ src/views/crossreference/requirements.ts
@@ -320,12 +320,17 @@ export const projectDocumentation: Requirements = {
     {
       id: "Web Project Proposal",
       description: "pdf",
-      src: "/documents/resume.pdf",
+      src: "/documents/web_project_proposal.pdf",
     },
     {
       id: "Web Project Documentation",
       description: "pdf",
-      src: "/documents/resume.pdf",
+      src: "/documents/web_project_documentation.pdf",
+    },
+    {
+      id: "Home Page Layout Sketch",
+      description: "pdf",
+      src: "/documents/home_layout_sketch.pdf",
     },
   ],
 };
